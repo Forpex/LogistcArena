53468384c1f7aa9fb1c47845687ca90d297a6776
Andreas Stock <aeypex@unterderbruecke.de>
2018-02-28 11:42:18 +0100

    time merge conflict solved

--- gamecore/Time.java
+++ gamecore/Time.java
@@ -3,21 +3,11 @@
  */
 package gamecore;
 
-<<<<<<< HEAD
 
-import gfx.Displayable;
-
-=======
->>>>>>> branch 'master' of https://github.com/Forpex/LogisticArena.git
 /**
  * @author Andreas Stock
  *
  */
 public class Time {
 
-<<<<<<< HEAD
-	
-	
-=======
->>>>>>> branch 'master' of https://github.com/Forpex/LogisticArena.git
 }
