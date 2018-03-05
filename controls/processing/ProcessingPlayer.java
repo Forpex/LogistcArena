ec525b0e58c3d906ff738c2d9265b4501f7f84eb
Andreas Stock <aeypex@unterderbruecke.de>
2018-03-05 21:43:13 +0100

    processing player

---
+++ controls/processing/ProcessingPlayer.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package controls.processing;
+
+import controls.Player;
+import gamecore.Intel;
+
+/**
+ * @author Andreas Stock
+ *
+ */
+public class ProcessingPlayer extends Player{
+
+	public ProcessingPlayer(int id) {
+		super(id);
+	}
+
+	@Override
+	public void post(Intel intel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
